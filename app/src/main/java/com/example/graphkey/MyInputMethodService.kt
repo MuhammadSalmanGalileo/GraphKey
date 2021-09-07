ae2bc50b35a192274c2f958de140db02663aa51f
Muhammad Salman Galileo <salman@pop-os.localdomain>
2021-09-07 20:41:45 +0800

    sucessfully create keyboard ui

--- app/src/main/java/com/example/graphkey/MyInputMethodService.kt
+++ app/src/main/java/com/example/graphkey/MyInputMethodService.kt
@@ -1,7 +1,23 @@
 package com.example.graphkey
 
 import android.inputmethodservice.InputMethodService
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputConnection
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MyInputMethodService: InputMethodService() {
 
+    override fun onCreateInputView(): View {
+        return layoutInflater.inflate(R.layout.keyboard_view, null).also{
+            var button: Button = it.findViewById(R.id.testButton)
+            button.setOnClickListener {
+                currentInputConnection.also { ic: InputConnection ->
+                    ic.commitText("Hello World", 1)
+                }
+                Log.d("test", "Button clicked");
+            }
+        }
+    }
 }
